4eb074f112c7c11de2e4be31734b939081cdae19
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-21 11:32:06 -0300

    Requisito 4 finalizado e comentado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -30,3 +30,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log (media);
 
+//Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, 
+//imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+// let media = 0;
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1){
+  
+//   soma = soma + numbers[i];
+//   media = soma / numbers.length;
+  
+// }
+
+// if (media > 20){
+//   console.log('Valor maior que 20')
+// }else{
+// console.log('Valor menor ou igual a 20')
+// }
+
+
+
+
