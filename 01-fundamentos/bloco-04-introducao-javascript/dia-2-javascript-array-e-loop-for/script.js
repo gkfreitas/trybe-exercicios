6217d9d9b66afc7d5df8fb8cf55ddfc1a96ced16
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-21 11:26:26 -0300

    Requisito 3 finalizado e comentado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -15,3 +15,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(soma);
 // }
 
+//Calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+// let media = 0;
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1){
+  
+//   soma = soma + numbers[i];
+//   media = soma / numbers.length;
+  
+// }
+
+// console.log (media);
+
