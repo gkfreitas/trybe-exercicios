c7c352ded3257c5ef440e9159687e02b17cfa04a
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-21 11:19:23 -0300

    Requsito 2 finalizado e comentado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -6,3 +6,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(numbers[i]);
 // }
 
+//Some todos os valores contidos no array e imprima o resultado;
+
+// let soma = 0;
+
+// for (i = 0; i < numbers.length; i += 1){
+//   soma = soma + numbers[i];
+//   console.log(soma);
+// }
+
