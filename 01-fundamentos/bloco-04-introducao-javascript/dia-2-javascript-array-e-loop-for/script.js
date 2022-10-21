3e50c587f1809ffd192af0274547c20e5b6298c6
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-21 16:04:04 -0300

    Requsito 8 adicionado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -84,7 +84,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(menorValor);
 
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 
+let array = [];
+
+for (let adiciona = 1; adiciona <= 25; adiciona += 1){
+  array.push(adiciona);
+}
+
+console.log(array);
 
 
 
