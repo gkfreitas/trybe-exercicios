e515fac339d390779e7941d0249268768e320fc2
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 13:27:37 -0300

    Iniciando exercicio 4

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -65,3 +65,7 @@
 // }
 
 // console.log(maiorNumero());
+
+//Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, 
+//e caso não seja nem positivo e nem negativo retorne “zero”.
+
