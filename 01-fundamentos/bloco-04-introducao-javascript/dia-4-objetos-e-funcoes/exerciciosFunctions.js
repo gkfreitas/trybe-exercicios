ee58e823394d47801cd30db1d6a8b5259512a6af
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 11:34:08 -0300

    exercicio 1 finalizado

---
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -0,0 +1,30 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, 
+//definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 5;
+const b = 3;
+
+function adicao() {
+    return a + b;
+}
+
+function subtracao() {
+    return a - b;
+}
+
+function multiplicacao() {
+    return a * b;
+}
+
+function divisao() {
+    return a / b;
+}
+
+function modulo() {
+    return a % b;
+}
