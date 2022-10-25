f471572399add81b8e130e5aac3a55cac96ab2e9
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 11:34:47 -0300

    Iniciando exercicio 2

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -6,25 +6,27 @@
 // Divisão (a / b)
 // Módulo (a % b)
 
-const a = 5;
-const b = 3;
+// const a = 5;
+// const b = 3;
 
-function adicao() {
-    return a + b;
-}
+// function adicao() {
+//     return a + b;
+// }
 
-function subtracao() {
-    return a - b;
-}
+// function subtracao() {
+//     return a - b;
+// }
 
-function multiplicacao() {
-    return a * b;
-}
+// function multiplicacao() {
+//     return a * b;
+// }
 
-function divisao() {
-    return a / b;
-}
+// function divisao() {
+//     return a / b;
+// }
 
-function modulo() {
-    return a % b;
-}
+// function modulo() {
+//     return a % b;
+// }
+
+//Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
