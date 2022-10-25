9a1db14ac04238cee82928d987b08d68467dccea
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 17:21:38 -0300

    Exercicio 1 finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -1,6 +1,23 @@
+// Exercicio 1 -
+
 // Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 // Exemplo de palíndromo: arara
 // verificaPalindrome('arara')
 // Retorno esperado: true
 // verificaPalindrome('desenvolvimento')
 // Retorno esperado: false
+
+let palindromoTrue = ''; 
+function verificaPalindrome(palindromo){
+		for (let i = 0; i < palindromo.length; i += 1) {
+			palindromoTrue += palindromo[palindromo.length - 1 - i];
+	}
+	if (palindromo == palindromoTrue){
+		return true;
+	}else{
+		return false;
+	}
+} 
+
+console.log(verificaPalindrome('cabeca'));
+  
