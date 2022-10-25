85e1896e20005f3f7f8b2048f41d7de87495bf39
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 13:53:15 -0300

    Exercicio 3 parte 1 finalizada

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
@@ -28,3 +28,16 @@
 // origem
 // nota
 // recorrente
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorente: 'sim'
+};
+
+for (let key in info){
+  console.log(key);
+}
+
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
