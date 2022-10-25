93bc9a862b0d05707e836832b29eb8ce46e72d9c
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 11:15:02 -0300

    Iniciando exercicio 2 pratica

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/parafixar.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/parafixar.js
@@ -1,18 +1,30 @@
 // Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
 
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3
-    }
+// let player = {
+// 	name: 'Marta',
+// 	lastName: 'Silva',
+// 	age: 34,
+// 	medals: {
+// 		golden: 2,
+// 		silver: 3
+// 	}
+// }
+
+// console.table(player);
+
+//Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let key in names){
+	console.log(key);
 }
-
-console.table(player);
