55150dd976cc1ab13b5e4ea046ee2ca4a7c52fb5
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 18:40:09 -0300

    Iniciando exercicio 4

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -50,17 +50,23 @@
 // Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 // Valor esperado no retorno da função: 6.
 
-function menorNumero(index){
+// function menorNumero(index){
 
-	let menorValor = index[0];
+// 	let menorValor = index[0];
 
-	for (i = 0; i < index.length; i += 1) {
+// 	for (i = 0; i < index.length; i += 1) {
 
-		if (menorValor > index[i]){
-			menorValor = index[i];
-		}
-	}
-	return menorValor;
-}
+// 		if (menorValor > index[i]){
+// 			menorValor = index[i];
+// 		}
+// 	}
+// 	return menorValor;
+// }
 
-console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]));
+// console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]));
+
+// Exercicio 4 -
+
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+// Valor esperado no retorno da função: Fernanda.
