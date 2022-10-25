258aaaef8453249186520318d97c54c7d1d4dfd1
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 13:50:51 -0300

    Exercicio 2 finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
@@ -1,10 +1,24 @@
 // Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
 // Bem-vinda, Margarida
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
 
-console.log('Bem-vinda, ' + info.personagem);
+// console.log('Bem-vinda, ' + info.personagem);
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. 
+//Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorente: 'sim'
+// };
+
+
+
+// console.log(info['recorente']);
