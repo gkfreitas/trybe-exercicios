76d25f1ae4d257702549ce68c05d5b6ab3c8456f
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 13:29:04 -0300

    Exercicio 4 finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -69,3 +69,14 @@
 //Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, 
 //e caso não seja nem positivo e nem negativo retorne “zero”.
 
+function avaliador(numero) {
+  if (numero > 0){
+    return 'Número positivo'
+  }else if (numero < 0){
+    return 'Número negativo'
+  }else{
+    return 'Zero, numero neutro'
+  }
+}
+
+console.log(avaliador(0))
