574305a61e2cd09fd59a6416da137a837d64bf2e
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 11:19:15 -0300

    Exercicio 3 finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/parafixar.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/parafixar.js
@@ -17,14 +17,26 @@
 
 // console.table(player);
 
-//Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 
-let names = {
-  person1: 'João',
-  person2: 'Maria',
-  person3: 'Jorge',
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge',
+// };
+
+// for (let key in names){
+// 	console.log(`Olá ${names[key]}`);
+// }
+
+// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
 };
 
-for (let key in names){
-	console.log(`Olá ${names[key]}`);
+for (let values in car){
+	console.log(values + ': ' + car[values]);
 }
