0925feb434a57ad0556fa94717dab6194bf09d4a
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-25 13:56:33 -0300

    Exercicio 3 finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosObjetos.js
@@ -36,8 +36,21 @@ let info = {
   recorente: 'sim'
 };
 
-for (let key in info){
-  console.log(key);
-}
+// for (let key in info){
+//   console.log(key);
+// }
 
 // Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+// for (let key in info){
+//   console.log(info[key]);
+// }
+
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, 
+//‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, 
+//de acordo com cada uma das chaves. Valor esperado no console:
+
