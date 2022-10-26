d49853b56ecd5ce194284dbba7d91fb4edc2240c
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-26 19:13:22 -0300

    Iniciando exercicio 1

---
+++ 01-fundamentos/bloco-04-introducao-javascript/exercicios.js
@@ -0,0 +1,7 @@
+// Exercicio 1 -
+
+// // Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
+1// Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo,
+//  ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+//  Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
+//  Copie o código abaixo e rode-o para verificar sua saída:
