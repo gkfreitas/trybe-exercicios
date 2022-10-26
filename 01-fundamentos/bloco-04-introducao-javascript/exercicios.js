6f83924a8b056a50e7716a0a1e712d77a3c1a23d
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-26 19:14:39 -0300

    Exercicio finalizado

--- 01-fundamentos/bloco-04-introducao-javascript/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-javascript/exercicios.js
@@ -5,3 +5,16 @@
 //  ela não deve ser acessível fora do escopo em que esteja sendo declarada.
 //  Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
 //  Copie o código abaixo e rode-o para verificar sua saída:
+
+    // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+    let pessoa = {
+      nome: 'Henri',
+      idade: 20
+    }
+    pessoa = {
+      nome: 'Luna',
+      idade: 19
+    } // Altere essa estrutura para corrigir o erro.
+
+    console.log('Nome:', pessoa.nome);
+    console.log('Idade:', pessoa.idade);
