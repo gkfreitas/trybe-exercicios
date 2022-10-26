07049698bafa851c9fb523cb426bf33345de0776
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-26 19:15:55 -0300

    Iniciando exercicio 2

--- 01-fundamentos/bloco-04-introducao-javascript/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-javascript/exercicios.js
@@ -6,15 +6,27 @@
 //  Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
 //  Copie o código abaixo e rode-o para verificar sua saída:
 
-    // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
-    let pessoa = {
-      nome: 'Henri',
-      idade: 20
-    }
-    pessoa = {
-      nome: 'Luna',
-      idade: 19
-    } // Altere essa estrutura para corrigir o erro.
+    // // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+    // let pessoa = {
+    //   nome: 'Henri',
+    //   idade: 20
+    // }
+    // pessoa = {
+    //   nome: 'Luna',
+    //   idade: 19
+    // } // Altere essa estrutura para corrigir o erro.
 
-    console.log('Nome:', pessoa.nome);
-    console.log('Idade:', pessoa.idade);
+    // console.log('Nome:', pessoa.nome);
+    // console.log('Idade:', pessoa.idade);
+
+// Exercicio 2 -
+
+// 🚀 Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
+
+// 🚀 Transforme a função numeroAleatorio em uma arrow function;
+
+    function numeroAleatorio() {
+      return Math.random()
+    }
+    console.log(numeroAleatorio());
+
