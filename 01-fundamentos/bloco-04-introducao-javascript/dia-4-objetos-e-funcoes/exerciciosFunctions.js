2de93e2a203591a3139923497651cbd1973b34f8
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-26 15:26:13 -0300

    Teste

--- 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/exerciciosFunctions.js
@@ -93,8 +93,6 @@
 
 // Exercicio - 5
 
-
-
 // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2
