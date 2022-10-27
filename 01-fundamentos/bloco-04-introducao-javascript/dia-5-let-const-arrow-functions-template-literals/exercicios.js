016c67ae2cc6203d43808949793b8d47f265b7d3
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-27 10:59:02 -0300

    Iniciando exercicio 6

--- 01-fundamentos/bloco-04-introducao-javascript/dia-5-let-const-arrow-functions-template-literals/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-5-let-const-arrow-functions-template-literals/exercicios.js
@@ -50,12 +50,20 @@
 // Exercicio 5 -
 //Altere a expressão if/else utilizando ternary operator;
 
-let speed = 90;
+// let speed = 90;
 
-  const speedCar = (speed) => {
-   return speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+//   const speedCar = (speed) => {
+//    return speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
 
-  };
+//   };
   
-  console.log(speedCar(speed));
+//   console.log(speedCar(speed));
 
+  // Exercicio 6 -
+
+  Crie uma função que ligue e desligue um motor de um carro.
+  Crie uma variável para armazenar o valor de status do seu motor (lembre-se de armazenar o tipo de variável da forma correta);
+  Crie a função ligarDesligar utilizando arrow functions. Ela não receberá nenhum parâmetro;
+  Crie a lógica para ligar e desligar o motor (a lógica deverá utilizar uma condicional para ligar e desligar o motor);
+  Imprima no terminal “O motor está ligado“ e “O motor está desligado“ (lembre-se de utilizar o template literals nesse momento).
+  Bônus (opcional): tente fazer o mesmo exercício utilizando ternary operator.
