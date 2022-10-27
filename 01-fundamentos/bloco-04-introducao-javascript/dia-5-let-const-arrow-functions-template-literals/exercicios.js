c3d7df8109ca14e4b01511122dbe77fcc491e72b
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-10-27 11:17:47 -0300

    Iniciando exercicio 7

--- 01-fundamentos/bloco-04-introducao-javascript/dia-5-let-const-arrow-functions-template-literals/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-5-let-const-arrow-functions-template-literals/exercicios.js
@@ -68,14 +68,29 @@
   // Imprima no terminal “O motor está ligado“ e “O motor está desligado“ (lembre-se de utilizar o template literals nesse momento).
   // Bônus (opcional): tente fazer o mesmo exercício utilizando ternary operator.
 
-let carro = 'desligado';
+// let carro = 'desligado';
 
-const ligaDesliga = () => {
-  carro === 'desligado' ? carro = 'ligado' : carro = 'desligado';
-  return carro;
-}
+// const ligaDesliga = () => {
+//   carro === 'desligado' ? carro = 'ligado' : carro = 'desligado';
+//   return carro;
+// }
 
-console.log(`O motor está ${ligaDesliga()}`);
-console.log(`O motor está ${ligaDesliga()}`);
-console.log(`O motor está ${ligaDesliga()}`);
-console.log(`O motor está ${ligaDesliga()}`);
+// console.log(`O motor está ${ligaDesliga()}`);
+// console.log(`O motor está ${ligaDesliga()}`);
+// console.log(`O motor está ${ligaDesliga()}`);
+// console.log(`O motor está ${ligaDesliga()}`);
+
+// Crie uma função que calcule a área de um círculo.
+// Na matemática, a fórmula para calcular a área de um círculo é: área = PI vezes o raio elevado ao quadrado.
+// Com base nessa informação:
+// Crie a função circleArea, que recebe o valor do raio como parâmetro, utilizando arrow functions;
+// Crie uma variável para armazenar o valor de PI (lembre-se de armazenar o tipo de variável da forma correta);
+// Crie a lógica para retornar a área do círculo;
+// Imprima no terminal “Essa é a área do círculo: resultado da função“ (lembre-se de utilizar o template literals nesse momento).
+// Exemplo de retorno:
+// Parâmetro: 3
+// Retorno: Essa é a área do círculo: 28.259999999999998
+// Parâmetro: 5
+// Retorno: Essa é a área do círculo: 78.5
+
+
