2e5fd9d6f40cbafd7c3f199c97534c26b0e866a6
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-01 14:42:03 -0300

    Função para alterar li criada

--- 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-03-javascript-eventos/mainPratica.js
+++ 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-03-javascript-eventos/mainPratica.js
@@ -11,6 +11,11 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 //  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+firstLi.addEventListener("click", clickTech);
+function clickTech() {
+  firstLi.classList.toggle('tech');
+}
+ 
 // - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
