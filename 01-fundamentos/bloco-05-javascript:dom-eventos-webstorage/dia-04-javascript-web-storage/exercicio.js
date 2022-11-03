bd9ebb6b10bff79e030ac1a6333b4aec42cd96a3
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-03 17:29:20 -0300

    Função principal feita

--- 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-04-javascript-web-storage/exercicio.js
+++ 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-04-javascript-web-storage/exercicio.js
@@ -1,1 +1,22 @@
-const button = document.querySelector('button');
+const buttonBackground = document.getElementById('option-background-color');
+const buttonColor = document.getElementById('option-color');
+const buttonFontSize = document.getElementById('option-font-size');
+const buttonLineHeight = document.getElementById('option-line-height');
+const buttonFontFamily = document.getElementById('option-font-family')
+const text = document.getElementById('important-text');
+
+buttonBackground.addEventListener("click", (event) => {
+  const eventTarget = event.target;
+  const eventValue = eventTarget.innerHTML;
+  text.style.backgroundColor = eventValue;
+});
+
+
+
+
+
+// for (i = 0; i < button.length; i += 1) {
+//   for (i2 = 0;  i2 < button[i].children.length; i2 += 1) {
+//     console.log(button[i].children[i2].innerHTML);
+//   }
+// }
