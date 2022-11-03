748196ae948e8099437b6909260d06111b634e53
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-03 20:01:18 -0300

    Melhorias feitas

--- 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-04-javascript-web-storage/exercicio.js
+++ 01-fundamentos/bloco-05-javascript:dom-eventos-webstorage/dia-04-javascript-web-storage/exercicio.js
@@ -8,37 +8,60 @@ const text = document.getElementById('important-text');
 buttonBackground.addEventListener("click", (event) => {
   const eventTarget = event.target;
   const eventValue = eventTarget.innerHTML;
-  text.style.backgroundColor = eventValue;
+  const backgroundColor = text.style.backgroundColor;
+  text.style.backgroundColor = eventValue
+  localStorage.setItem('backgroundColor', JSON.stringify(eventValue));
 });
+
+console.log(localStorage.getItem('backgroundColor'));
 
 buttonColor.addEventListener("click", (event) => {
   const eventTarget = event.target;
   const eventValue = eventTarget.innerHTML;
   text.style.color = eventValue;
+  localStorage.setItem('color', JSON.stringify(eventValue));
 });
 
 buttonFontSize.addEventListener("click", (event) => {
   const eventTarget = event.target;
   const eventValue = eventTarget.innerHTML;
   text.style.fontSize = eventValue;
+  localStorage.setItem('fontSize', JSON.stringify(eventValue));
 });
 
 buttonLineHeight.addEventListener("click", (event) => {
   const eventTarget = event.target;
   const eventValue = eventTarget.innerHTML;
   text.style.lineHeight = eventValue;
+  localStorage.setItem('lineHeight', JSON.stringify(eventValue));
 });
 
 buttonFontFamily.addEventListener("click", (event) => {
   const eventTarget = event.target;
   const eventValue = eventTarget.innerHTML;
   text.style.fontFamily = eventValue;
+  localStorage.setItem('fontFamily', JSON.stringify(eventValue));
+  
 });
 
+const clearFilter = document.getElementById('clear-filter');
+clearFilter.addEventListener("click", () => {
+  text.style.backgroundColor =  'initial';
+  text.style.color = 'initial';
+  text.style.fontSize = 'initial';
+  text.style.lineHeight = 'initial';
+  text.style.fontFamily = 'initial';
+  localStorage.setItem('backgroundColor','initial');
+  localStorage.setItem('color', 'initial');
+  localStorage.setItem('fontSize', 'initial');
+  localStorage.setItem('lineHeight', 'initial');
+  localStorage.setItem('fontFamily', 'initial');
+});
 
-
-// for (i = 0; i < button.length; i += 1) {
-//   for (i2 = 0;  i2 < button[i].children.length; i2 += 1) {
-//     console.log(button[i].children[i2].innerHTML);
-//   }
-// }
+window.onload = function() {
+  text.style.backgroundColor = JSON.parse(localStorage.getItem('backgroundColor'));
+  text.style.color = JSON.parse(localStorage.getItem('color'));
+  text.style.fontSize = JSON.parse(localStorage.getItem('fontSize'));
+  text.style.lineHeight = JSON.parse(localStorage.getItem('lineHeight'));
+  text.style.fontFamily = JSON.parse(localStorage.getItem('fontFamily'));
+}
