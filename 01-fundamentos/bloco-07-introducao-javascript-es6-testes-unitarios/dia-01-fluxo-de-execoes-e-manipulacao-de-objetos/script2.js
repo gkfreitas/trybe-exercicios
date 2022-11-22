17cd0d90f74a05c75b4b1276ee71d9379b30774c
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-22 11:08:45 -0300

    Exercicios finalizados

--- 01-fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-fluxo-de-execoes-e-manipulacao-de-objetos/script2.js
+++ 01-fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-fluxo-de-execoes-e-manipulacao-de-objetos/script2.js
@@ -26,6 +26,33 @@ function listKeys(object) {
   return Object.keys(object);
 }
 
+function objectLength(object) {
+  return Object.keys(object).length;
+}
+
+function objectList(object) {
+  return Object.values(object);
+}
+
+const allLessons = {
+  lesson1: {},
+  lesson2: {},
+  lesson3: {}
+};
+
+
 changeObject(lesson2, 'turno', 'noite');
 
-console.log(listKeys(lesson1));
+const allLessonsLength = Object.keys(allLessons).length;
+const allObjects = [lesson1, lesson2, lesson3];
+for (let i = 1; i <= allLessonsLength; i+= 1) {
+  Object.assign(allLessons[`lesson${i}`], allObjects[i - 1]);
+}
+
+function getValueByNumber(object, index) {
+  return Object.values(object)[index];
+}
+
+function verifyPair(object, key, value) {
+  
+}
