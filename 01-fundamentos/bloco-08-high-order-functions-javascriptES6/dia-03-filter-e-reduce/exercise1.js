f195c43b3e7e36b9ffad8b50a322782e6515482b
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-01 15:06:59 -0300

    Parte 2 exercício finalizado.

--- 01-fundamentos/bloco-08-high-order-functions-javascriptES6/dia-03-filter-e-reduce/exercise1.js
+++ 01-fundamentos/bloco-08-high-order-functions-javascriptES6/dia-03-filter-e-reduce/exercise1.js
@@ -63,40 +63,75 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
+// const expectedResult = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: { name: 'George R. R. Martin', birthYear: 1948 },
+//     releaseYear: 1991
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Frank Herbert', birthYear: 1920 },
+//     releaseYear: 1965
+//   }
+// ];
+
+// const fantasyOrScienceFiction = (array) => {
+//   // escreva seu código aqui
+//   return array.filter((element) => element.genre === 'Ficção Científica' || element.genre === 'Fantasia');
+// }
+
+// console.log(fantasyOrScienceFiction(books));
+
 const expectedResult = [
   {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
     author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
+    releaseYear: 1951,
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
 ];
 
-const fantasyOrScienceFiction = (array) => {
+const oldBooksOrdered = (array) => {
   // escreva seu código aqui
-  return array.filter((element) => element.genre === 'Ficção Científica' || element.genre === 'Fantasia');
+  const filterAge = array.filter((element) => {
+    const bookAge = 2022 - element.releaseYear;
+    return bookAge > 60;
+  });
+  return filterAge.sort((a,b) => a.releaseYear - b.releaseYear)
 }
 
-console.log(fantasyOrScienceFiction(books));
+console.log(oldBooksOrdered(books));
