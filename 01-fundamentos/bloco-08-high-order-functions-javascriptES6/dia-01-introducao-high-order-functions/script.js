7a160d261b304d4038e88c3120e6bd2f3e3b1af0
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-02 17:00:59 -0300

    Exercicio 1 finalizado

---
+++ 01-fundamentos/bloco-08-high-order-functions-javascriptES6/dia-01-introducao-high-order-functions/script.js
@@ -0,0 +1,15 @@
+const generateEmail = (fullname) => {
+  const email = fullname.replace(' ', '_').toLowerCase();
+  return {
+    nome: fullname, email: `${email}@trybe.com`,
+  }
+}
+const newEmployees = (generate) => {
+  const employees = {
+    id1: generate('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: generate('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: generate('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+console.log(newEmployees(generateEmail));
