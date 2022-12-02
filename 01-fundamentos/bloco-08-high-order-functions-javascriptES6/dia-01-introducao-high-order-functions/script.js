9bbc9558bf28798f3d27efcffc99d7add20054d3
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-02 17:08:35 -0300

    Exercicio 2 finalizado

--- 01-fundamentos/bloco-08-high-order-functions-javascriptES6/dia-01-introducao-high-order-functions/script.js
+++ 01-fundamentos/bloco-08-high-order-functions-javascriptES6/dia-01-introducao-high-order-functions/script.js
@@ -1,15 +1,29 @@
-const generateEmail = (fullname) => {
-  const email = fullname.replace(' ', '_').toLowerCase();
-  return {
-    nome: fullname, email: `${email}@trybe.com`,
-  }
-}
-const newEmployees = (generate) => {
-  const employees = {
-    id1: generate('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: generate('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: generate('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
-};
-console.log(newEmployees(generateEmail));
+// const generateEmail = (fullname) => {
+//   const email = fullname.replace(' ', '_').toLowerCase();
+//   return {
+//     nome: fullname, email: `${email}@trybe.com`,
+//   }
+// }
+// const newEmployees = (generate) => {
+//   const employees = {
+//     id1: generate('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: generate('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: generate('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   }
+//   return employees;
+// };
+// console.log(newEmployees(generateEmail));
+
+// const generateNumber = () => {
+//   const generate = Math.floor(Math.random () * (6 - 1) + 1)
+//   return generate;
+// };
+
+// const checkRandomNumber = (number) => {
+//   if (number === generateNumber()) {
+//     return 'Parabéns, você ganhou';
+//   }
+//   return 'Tente novamente';
+// }
+
+// console.log(checkRandomNumber(5));
