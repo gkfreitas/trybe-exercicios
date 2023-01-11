e33fc602f188d22bebf6bb52991e20ecd22564a6
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-11 15:12:01 -0300

    Não deu certo

--- 02-front-end/bloco-02-javascript-assincrono-promises-e-fetch/microblog/index.js
+++ 02-front-end/bloco-02-javascript-assincrono-promises-e-fetch/microblog/index.js
@@ -1,23 +1,5 @@
-import './style.css';
-
-import {
-  fillUsersSelect,
-  fillPosts,
-  fillFeaturedPostComments,
-  clearPageData,
-  fillErrorMessage,
-} from './utils/updateUI';
-
-const usersSelect = document.querySelector('#users-select');
-
 const USERS_API = 'https://dummyjson.com/users';
-// faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
-fetch(USERS_API)
+
+const users = fetch(USERS_API)
   .then((response) => response.json())
   .then((response) => console.log(response));
-
-usersSelect.addEventListener('change', () => {
-  clearPageData();
-
-  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
-});
