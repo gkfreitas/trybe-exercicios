e5757b7fb6ca99cfcd067a85d323f732b529336f
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-12 13:11:14 -0300

    Microblog finalziado

--- 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/microblog/index.js
+++ 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/microblog/index.js
@@ -19,6 +19,16 @@ fetch(USERS_API)
 
 usersSelect.addEventListener('change', () => {
   clearPageData();
+  fetch(`https://dummyjson.com/posts/user/${usersSelect.value}`)
+  .then((response) => response.json())
+  .then((data) => {
+    fillPosts(data.posts);
+    const [featuredPost] = data.posts;
+    return fetch(`https://dummyjson.com/posts/${featuredPost.id}/comments`)
+  })
+  .then((coment) => coment.json())
+  .then((data) => fillFeaturedPostComments(data.comments))
+  .catch((error) => fillErrorMessage(error));
 
   // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
 });
