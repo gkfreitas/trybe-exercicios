57a08399bb52e2561373985747c0227af216a858
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-12 11:12:07 -0300

    Clipboard copy adicionado

--- 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/src/index.js
+++ 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/src/index.js
@@ -1,4 +1,6 @@
 import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
 
 const buttonGenerator = document.querySelector('button');
 const passwordText = document.querySelector('h2');
@@ -6,4 +8,5 @@ const passwordText = document.querySelector('h2');
 buttonGenerator.addEventListener('click', () => {
     const randomPassword = nanoid();
     passwordText.innerHTML = randomPassword;
+    copy(randomPassword);
 });
