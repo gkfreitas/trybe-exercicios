9d1d8ad1be6da5255ffbd83e889e617a49afc73f
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-12 10:29:08 -0300

    ESLint padrão trybe adicionado.

--- 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/src/index.js
+++ 02-front-end/bloco-01-introducao-frontend-e-javascript-assincrono/01-ambiente-de-desenvolvimento/src/index.js
@@ -4,6 +4,6 @@ const buttonGenerator = document.querySelector('button');
 const passwordText = document.querySelector('h2');
 
 buttonGenerator.addEventListener('click', () => {
-  const randomPassword = nanoid();
-  passwordText.innerHTML = randomPassword;
+    const randomPassword = nanoid();
+    passwordText.innerHTML = randomPassword;
 });
