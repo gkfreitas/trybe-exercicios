b26bade2b2594fcd5242bc573857a3a5431f14cb
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-25 10:44:30 -0300

    Exercicio fancy-buttons finalizado

--- 02-front-end/bloco-03-componentes-com-estado-e-eventos/01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
+++ 02-front-end/bloco-03-componentes-com-estado-e-eventos/01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const message = () => {
+  console.log('Click');
+}
+
+const message2 = () => {
+  console.log('Click2');
+}
+
+const message3 = () => {
+  console.log('Click3');
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={message}>Texto 1</button>
+      <button onClick={message2}>Texto 2</button>
+      <button onClick={message3}>Texto 3</button>
+    </>
   );
 }
 
