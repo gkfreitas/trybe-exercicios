0ec9537bc0545b29090c74126046fe794c7d8763
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-02-16 14:05:54 -0300

    Exercicio de fixação finalizado

--- 02-front-end/bloco-06-testes-automatizados-com-react-testing-libary/02-mocks-e-inputs/rtl-mocks-inputs/src/App.test.js
+++ 02-front-end/bloco-06-testes-automatizados-com-react-testing-libary/02-mocks-e-inputs/rtl-mocks-inputs/src/App.test.js
@@ -1,5 +1,6 @@
 // App.test.js
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from './App';
 
@@ -12,17 +13,33 @@ it('fetches a joke', async () => {
     status: 200,
   };
 
+  const joke2 = {
+    id: 'xXSv492wPmb',
+    joke: 'What is red and smells like blue paint? Red paint!',
+    status: 200,
+  };
+
+
   jest.spyOn(global, 'fetch');
-  global.fetch.mockResolvedValue({
+  global.fetch.mockResolvedValueOnce({
     json: jest.fn().mockResolvedValue(joke),
   });
 
   render(<App />);
-  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
-  expect(renderedJoke).toBeInTheDocument();
-  expect(global.fetch).toHaveBeenCalledTimes(1);
-  expect(global.fetch).toHaveBeenCalledWith(
-    'https://icanhazdadjoke.com/',
-    { headers: { Accept: 'application/json' } },
-  );
+  const button = screen.getByRole('button');
+
+  expect(await screen.findByText(joke.joke)).toBeInTheDocument();
+  expect(screen.queryByText(joke2.joke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(joke2),
+  });
+
+  userEvent.click(button);
+  
+  expect(await screen.findByText(joke2.joke)).toBeInTheDocument();
+  expect(screen.queryByText(joke.joke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(2);
 });
