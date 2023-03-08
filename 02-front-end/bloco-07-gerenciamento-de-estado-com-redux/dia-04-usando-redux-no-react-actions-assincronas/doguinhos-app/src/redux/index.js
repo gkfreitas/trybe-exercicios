783223ee01305cb8d997bb39230745ea9a54f413
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-03-08 14:06:49 -0300

    Pratica finalizado

--- 02-front-end/bloco-07-gerenciamento-de-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
+++ 02-front-end/bloco-07-gerenciamento-de-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
@@ -1,8 +1,12 @@
-// ./src/redux/index.js
+// src/redux/index.js
 import { composeWithDevTools } from '@redux-devtools/extension';
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
 import dogReducer from './reducers/dogReducer';
 
-const store = createStore(dogReducer, composeWithDevTools());
+const store = createStore(
+  dogReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
